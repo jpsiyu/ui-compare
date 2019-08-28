1878c4e664832b3a5b56ed0d6a9895d19c1af1c5
wusiyu <stephenwu2019@163.com>
2019-08-28 16:29:33 +0800

    fix element import

--- src/assets/plugins/element.js
+++ src/assets/plugins/element.js
@@ -1,8 +1,16 @@
 import ElementUI from 'element-ui'
+import { Loading, MessageBox, Notification, Message } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 export default {
   install: function (Vue, options) {
     Vue.use(ElementUI)
+    Vue.prototype.$loading = Loading.service;
+    Vue.prototype.$msgbox = MessageBox;
+    Vue.prototype.$alert = MessageBox.alert;
+    Vue.prototype.$confirm = MessageBox.confirm;
+    Vue.prototype.$prompt = MessageBox.prompt;
+    Vue.prototype.$notify = Notification;
+    Vue.prototype.$message = Message
   }
 }
